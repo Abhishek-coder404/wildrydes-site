c168691ada5b068e95014bb590d89e7e42ea5398
Abhishek-coder404 <87005319+Abhishek-coder404@users.noreply.github.com>
2025-04-25 01:01:43 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_8jhBRt02E', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '33041m6d5uic9r3l4np0mt3kc2', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
